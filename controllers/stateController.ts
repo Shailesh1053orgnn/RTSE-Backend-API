d8a11993fa3a9007fb6fef3feb6de4303b4422e3
Amit Joshi <101185684+NMBP4180@users.noreply.github.com>
2023-03-09 11:29:50 +0530

    Update stateController.ts

--- controllers/stateController.ts
+++ controllers/stateController.ts
@@ -6,9 +6,9 @@ export class StateController {
         const state = new stateModel();
         const returnValue: IStateModel[] = await state.findAll();
         if (returnValue.length > 0) {
-            res.status(200).send({Response: { "status": 200, "success": true, data: returnValue }});
+            res.status(200).send({ Response: { "status": 200, "success": true, data: returnValue } });
         } else {
-            res.status(200).send({ Response: { "status": 200, "success": true, message: "Data Not found" }});
+            res.status(200).send({ Response: { "status": 200, "success": true, message: "Data Not found" } });
         }
     }
     static async findById(req: Request, res: Response) {
@@ -17,9 +17,9 @@ export class StateController {
             state_id: req.query.state_id
         });
         if (returnValue.length > 0) {
-            res.status(200).send({ Response:{ "status": 200, "success": true, data: returnValue }});
+            res.status(200).send({ Response: { "status": 200, "success": true, data: returnValue } });
         } else {
-            res.status(200).send({ Response:{ "status": 200, "success": false, message: "Data Not found" }});
+            res.status(200).send({ Response: { "status": 200, "success": true, message: "Data Not found" } });
         }
     }
 }
